6ae43ada75e99e21130295569e946bdb599fec46
Stefano Morgia <ste.morgia@gmail.com>
2022-07-15 15:08:06 +0200

    prove js

--- js/main.js
+++ js/main.js
@@ -1,9 +1,30 @@
 const containerDom = document.getElementById('container');
+const numeriContainer = document.getElementById('numeri');
+setTimeout (hide, 30000);
 
-setTimeout (hide, 3000);
+
+
+for (let i = 0; i < 5; i++) {
+    let numeriArray = [];
+    let numeri = numeriCasuali (50);
+    console.log(numeri);
+    numeriArray.push(numeri);
+    console.log (numeriArray);
+    numeriContainer.innerText = numeriArray;
+}
+
+
 
 
 
 function hide () {
     containerDom.classList.add('d-none');
 }
+
+function numeriCasuali (max) {
+    return Math.floor(Math.random() * max);
+}
+
+
+
+
